c9b7aadf79c013683daf82ad7d59f5223b321732
Allan Pereira <allan.pereira@edu.itescia.fr>
2022-06-20 15:27:27 +0200

    list word change to ascii list

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ def set_list_word(word):
     list_word = list(word)
     return list_word
 
+def ascii_list(list_word):
+
+    list_ascii = []
+    for i in list_word:
+        list_ascii.append(ord(i))
+    return list_ascii
+
 while True:
     # Main loop
     text = get_input()
     if text != "":
         list_word = set_list_word(text)
         print(list_word)
+        list_ascii = ascii_list(text)
+        print(list_ascii)
+
