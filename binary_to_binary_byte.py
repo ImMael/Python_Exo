46e283632594fa107a504ba3a7bd7050f71e0c0b
Nohan75 <nohanmarielouise@gmail.com>
2022-06-20 15:52:16 +0200

    add docstring to the function

--- binary_to_binary_byte.py
+++ binary_to_binary_byte.py
@@ -1,5 +1,9 @@
-# add 0 to transform binary to binary byte
 def binary_to_binary_byte(wordlist):
+    """
+    Converts a list of binary words to a list of binary bytes.
+    :param wordlist:
+    :return res:
+    """
     res = []
     for word in wordlist:
         if len(word) < 8:
