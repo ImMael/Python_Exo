57af11e1d8022e2daa0a339dc06ef83a7a632376
Merwan <merwanlaouini@gmail.com>
2022-06-20 15:53:12 +0200

    ajout des docStrings

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 def get_input():
-    # Get input from user
+    """
+    Get input from user
+    """
     return input("Enter a word: ")
 
 
 def set_list_word(word):
-    # Set list of word
+    """
+    Set list of word
+    """
     list_word = list(word)
     return list_word
 
 def ascii_list(list_word):
-
+    """
+    Convert list_word to list_ascii
+    """
     list_ascii = []
     for i in list_word:
         list_ascii.append(ord(i))
     return list_ascii
 
 def convert_bin(list_ascii):
+    """
+    Convert list_ascii to binary
+    """
     list_bin = []
     for i in list_ascii:
         list_bin.append(bin(i).replace("0b",""))
